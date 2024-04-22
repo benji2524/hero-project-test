c070502552279f2a080c709651097daa2a01ff06
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-04-21 23:11:51 -0400

    15 commit
    added enemy capsule, enemy mat, and enemy code.

--- Hero Born/Assets/Scripts/EnemyBehavior.cs
+++ Hero Born/Assets/Scripts/EnemyBehavior.cs
@@ -15,4 +15,20 @@ public class EnemyBehavior : MonoBehaviour
     {
         
     }
+    void OnTriggerEnter(Collider other)
+ {
+
+    if(other.name == "Player")
+    {
+        Debug.Log("Player detected - attack!");
+    }
+}
+ 
+ void OnTriggerExit(Collider other)
+ {
+    if(other.name == "Player")
+   {
+        Debug.Log("Player out of range, resume patrol");
+    }
+ }
 }
