b9501f3d8098b2265793b7a579c6fd022f69cd89
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-04-21 22:57:30 -0400

    QOL
    made code look nicer

--- Hero Born/Assets/Scripts/ItemBehavior.cs
+++ Hero Born/Assets/Scripts/ItemBehavior.cs
@@ -7,16 +7,16 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ItemBehavior : MonoBehaviour
 {
- // 1
+
  void OnCollisionEnter(Collision collision)
  {
- // 2
+
  if(collision.gameObject.name == "Player")
  {
- // 3
- Destroy(this.transform.gameObject);
- // 4
- Debug.Log("Item collected!");
+
+    Destroy(this.transform.gameObject);
+
+    Debug.Log("Item collected!");
  }
  }
 } 
