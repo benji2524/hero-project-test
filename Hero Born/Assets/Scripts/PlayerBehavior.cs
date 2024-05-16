ad26b2ddbe10163c5e4c27f34489643e4fadf32b
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-05-16 14:22:59 -0400

    16th commit
    added various comments to playerbehavior.cs

--- Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -10,9 +10,20 @@ public class PlayerBehavior : MonoBehaviour
     private float _hInput;
     private Rigidbody _rb;
     // Start is called before the first frame update
+    
+    // _vInput: stores vertical axis input
+    
+    //_hinput: stores horizantal axis input
+    
+    /*
+    private variable of type Rigidbody that will contain a reference to the capsule’s
+    Rigidbody component
+    
+    */
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        //e GetComponent method checks whether the component type we’re looking for, in this case, Rigidbody, exists on the GameObject the script is attached to and returns it
 
     }
 
@@ -21,12 +32,22 @@ public class PlayerBehavior : MonoBehaviour
     {
     _vInput = Input.GetAxis("Vertical") * MoveSpeed;
      _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+     /*
+     this.transform.Translate(Vector3.forward *_vInput *Time.deltaTime);
+     this.transform.Rotate(Vector3.up *_hInput *Time.deltaTime);
+      */
     
-    /*
-    this.transform.Translate(Vector3.forward *_vInput *Time.deltaTime);
-    this.transform.Rotate(Vector3.up *_hInput *Time.deltaTime);
-    */
-        
+    //Comments out the Transform and Rotate method c
+
+    //Movespeed: speed player moves forward or backwards
+    
+    //Rotatespeed: speed rotates left and right
+   
+   //Input.GetAxis("Vertical") detects when the up arrow, down arrow, W, or S key is pressed and multiplies that value by MoveSpeed:
+ 
+   //Input.GetAxis("Horizontal") detects when the left arrow, right arrow, A, or D key is pressed and multiplies that value by RotateSpeed:
+
+       
     }
     void FixedUpdate()
     {
@@ -37,5 +58,18 @@ public class PlayerBehavior : MonoBehaviour
     _rb.MovePosition(this.transform.position +  this.transform.forward * _vInput * Time.fixedDeltaTime);
 
     _rb.MoveRotation(_rb.rotation * angleRot);
+    //new Vector3 variable stores left and right rotations
+    
+    //Quaternion.Euler takes a Vector3 parameter and returns a rotation value in Euler angles
+    
+    /*
+    Calls MovePosition on our _rb component, which takes in a Vector3 parameter and
+    applies force 
+    
+    Calls the MoveRotation method on the _rb component, which also takes in a Vector3
+    parameter and applies applies force
+
+    */
+
     }
 }
