8bced710eb3db865d167febecb68ecd5659dbb5a
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-05-16 17:25:37 -0400

    21 commit
    added jumping code chap 8

--- Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -7,69 +7,94 @@ public class PlayerBehavior : MonoBehaviour
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
      private float _vInput;
+     
+     //_hinput: stores horizantal axis input
     private float _hInput;
-    private Rigidbody _rb;
-    // Start is called before the first frame update
-    
-    // _vInput: stores vertical axis input
     
     //_hinput: stores horizantal axis input
+    public float JumpVelocity = 5f;
+   
+    //holds force applied on jump
+    private bool _isJumping;
     
-    /*
+    //reports true/false depending on player jump
+
+    private Rigidbody _rb;
+   
+    // Start is called before the first frame update
+    
+     /*
     private variable of type Rigidbody that will contain a reference to the capsule’s
     Rigidbody component
     
     */
+    
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        //e GetComponent method checks whether the component type we’re looking for, in this case, Rigidbody, exists on the GameObject the script is attached to and returns it
+        
+        //GetComponent method checks whether the component type we’re looking for, in this case, Rigidbody, exists on the GameObject the script is attached to and returns it
 
     }
 
     // Update is called once per frame
+   
+   
     void Update()
     {
     _vInput = Input.GetAxis("Vertical") * MoveSpeed;
-     _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
-     /*
-     this.transform.Translate(Vector3.forward *_vInput *Time.deltaTime);
-     this.transform.Rotate(Vector3.up *_hInput *Time.deltaTime);
-      */
     
-    //Comments out the Transform and Rotate method c
-
     //Movespeed: speed player moves forward or backwards
     
-    //Rotatespeed: speed rotates left and right
-   
-   //Input.GetAxis("Vertical") detects when the up arrow, down arrow, W, or S key is pressed and multiplies that value by MoveSpeed:
- 
-   //Input.GetAxis("Horizontal") detects when the left arrow, right arrow, A, or D key is pressed and multiplies that value by RotateSpeed:
+    //Input.GetAxis("Vertical") detects when the up arrow, down arrow, W, or S key is pressed and multiplies that value by MoveSpeed
+    
+     _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+     
+     //Rotatespeed: speed rotates left and right
+    
+    //Input.GetAxis("Horizontal") detects when the left arrow, right arrow, A, or D key is pressed and multiplies that value by RotateSpeed:
 
-       
-    }
+    /*
+     this.transform.Translate(Vector3.forward *_vInput *Time.deltaTime);
+     this.transform.Rotate(Vector3.up *_hInput *Time.deltaTime);
+    */
+    //Comments out the Transform and Rotate method 
+    
+    _isJumping |= Input.GetKeyDown(KeyCode.J);
+   
+   //onlyfires once per frame and returns bool depending on key specified press
+   
+   //checks if j key pressed
+   }
+   
+   
     void FixedUpdate()
     {
     Vector3 rotation = Vector3.up * _hInput;
+    //new Vector3 variable stores left and right rotations
 
     Quaternion angleRot = Quaternion.Euler(rotation *Time.fixedDeltaTime);
+    //Quaternion.Euler takes a Vector3 parameter and returns a rotation value in Euler angles
 
     _rb.MovePosition(this.transform.position +  this.transform.forward * _vInput * Time.fixedDeltaTime);
 
     _rb.MoveRotation(_rb.rotation * angleRot);
-    //new Vector3 variable stores left and right rotations
-    
-    //Quaternion.Euler takes a Vector3 parameter and returns a rotation value in Euler angles
-    
-    /*
+     /*
     Calls MovePosition on our _rb component, which takes in a Vector3 parameter and
     applies force 
     
     Calls the MoveRotation method on the _rb component, which also takes in a Vector3
     parameter and applies applies force
-
     */
-
+    
+    if(_isJumping)
+    //checks if rumping true and activates jump animation
     }
+    {
+ 
+        _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+    }
+        _isJumping = false;
+   
+        //resets to false so code knows a jump has been completed.
 }
