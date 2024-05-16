9be88f4244a5458c76293041fe7a49eda794d6b9
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-05-16 14:43:48 -0400

    17th commit
    comments added to itembehavior

--- Hero Born/Assets/Scripts/ItemBehavior.cs
+++ Hero Born/Assets/Scripts/ItemBehavior.cs
@@ -10,13 +10,22 @@ public class ItemBehavior : MonoBehaviour
 
  void OnCollisionEnter(Collision collision)
  {
-
+   //another object runs into the Item Prefab, Unity automatically calls the OnCollisionEnter method
+   
+   //OnCollisionEnter comes with a parameter that stores a reference to the Collider that ran into it
+ 
  if(collision.gameObject.name == "Player")
  {
 
+   //Collision class has a property, called gameObject, that holds a reference to the colliding GameObject’s Collider
+    
     Destroy(this.transform.gameObject);
+   
+   //removes collectible on player condition
 
     Debug.Log("Item collected!");
+
+   //prints "item collected" on player collision
  }
  }
 } 
