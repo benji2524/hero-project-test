348723a0caa95d9b107ca5b5a202014a9b79cf46
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-05-16 14:58:31 -0400

    20th commit
    added comments to camerabehavior

--- Hero Born/Assets/Scripts/CameraBehavior.cs
+++ Hero Born/Assets/Scripts/CameraBehavior.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {   public Vector3 CamOffset= new Vector3(0f, 1.2f, -2.6f);
+//vector3 variable declared to store distance between camera and player
+
     private Transform _target;
 
     // Start is called before the first frame update
     void Start()
     {
         _target = GameObject.Find("Player").transform;
+        //used to locate the player
     }
 
     // Update is called once per frame
     void LateUpdate()
+    //executes after update
     {
         this.transform.position = _target.TransformPoint(CamOffset);
         this.transform.LookAt(_target);
+
+        //camera follows player
     }
 }
