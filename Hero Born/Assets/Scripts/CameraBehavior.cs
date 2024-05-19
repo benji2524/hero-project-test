d893a5db16f78a1281a0d1fbb1f58d716e48a03a
benji2524 <166969455+benji2524@users.noreply.github.com>
2024-05-19 13:58:25 -0400

    24th commit
    moved worlds to seperate folder for organization purposes.

--- Hero Born/Assets/Scripts/CameraBehavior.cs
+++ Hero Born/Assets/Scripts/CameraBehavior.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
-{   public Vector3 CamOffset= new Vector3(0f, 1.2f, -2.6f);
-//vector3 variable declared to store distance between camera and player
+{   //vector3 variable declared to store distance between camera and player
+    public Vector3 CamOffset= new Vector3(0f, 1.2f, -2.6f);
+
 
     private Transform _target;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   //used to locate the player
         _target = GameObject.Find("Player").transform;
-        //used to locate the player
+       
     }
 
     // Update is called once per frame
     void LateUpdate()
-    //executes after update
-    {
+    
+    {    //camera follows player
         this.transform.position = _target.TransformPoint(CamOffset);
         this.transform.LookAt(_target);
 
-        //camera follows player
+        
     }
 }
